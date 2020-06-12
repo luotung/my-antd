f7079d9718a9fd70b4cca08a87f84e5dfd868dbf
yhl <@>
2020-06-12 15:25:37 +0800

    左侧列表key重复导致点击是两个高亮

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -29,13 +29,13 @@ export default class BasicLayout extends Component {
                             <Menu.Item key="3"><Link to="/dashboard/monitor">监控页</Link></Menu.Item>
                             <Menu.Item key="4"><Link to="/dashboard/workplace">工作台</Link></Menu.Item>
                         </SubMenu>
-                        <Menu.Item key="2">
+                        <Menu.Item key="5">
                             <Link to="/puzzlecards">
                                 <Icon type="pie-chart" />
                                 <span>Puzzlecards</span>
                             </Link>
                         </Menu.Item>
-                        <Menu.Item key="3">
+                        <Menu.Item key="6">
                             <Link to="/list">
                                 <Icon type="pie-chart" />
                                 <span>List</span>
